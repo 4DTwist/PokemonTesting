eaacb1570fc0e3cd61cdccf6fb510c8ae85f3188
Joshua Levett <joshua.levett@live.co.uk>
2025-05-15 14:49:09 +0100

    Commiting a test to move towards completing. Test fails at present as the code written cannot account for a negative number.

--- PokemonTestingTests/Test1.cs
+++ PokemonTestingTests/Test1.cs
@@ -11,9 +11,25 @@ public sealed class Test1
     /// Check the base damage handler returns the value input.
     /// </summary>
     [TestMethod]
-    public void Test_BaseDamageHandler()
+    [DataRow(-99)]
+    [DataRow(-25)]
+    [DataRow(0)]
+    [DataRow(80)]
+    [DataRow(100)]
+    public void Test_BaseDamageHandler(int inputInt)
     {
+        int outputInt = inputInt;
+        if (outputInt < 0)
+        {
+            outputInt = 0;
+        }
+        //Expectations at this stage:
+        //ABS the number. Anything below 0 should be treated as 0.
+        //Return the same number. It is being multiplied by 1 inside the method.
+        //There will be further tests as the complexity of the system increases.
+        //We may run into issues using an Int as our numeric data type. Consider Double/Decimal etc. Probably Decimal.
+
         var x = new Mechanics.DamageHandler();
-        Assert.AreEqual(1, x.DamageCheck("Grass",1));
+        Assert.AreEqual(outputInt, x.DamageCheck("Grass", inputInt));
     }
 }
