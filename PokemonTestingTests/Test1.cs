46c4c3f9165e8e161c34c01e6d757ef9c8ecd5d0
Joshua Levett <joshua.levett@live.co.uk>
2025-05-18 07:28:10 +0100

    Added DamageTypeModifiers test case.

--- PokemonTestingTests/Test1.cs
+++ PokemonTestingTests/Test1.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using PokemonTesting;
+using static PokemonTesting.PokemonMonsters;
 
 
 namespace PokemonTestingTests;
@@ -44,16 +46,50 @@ public sealed class Test1
     [DataRow("Normal", 70)]
     [DataRow("Null", 70)]
     [DataRow(null, 70)]
-    [DataRow("",70)]
-    
+    [DataRow("", 70)]
+
     public void Test_AllTypesDamageHandler(string type, double inputInt)
     {
         //Expected output is identical.
         //Any unexpected should be considered typeless, and deal damage with no modifier.
+        //NOTE: The base DamageHandler is NOT the same as normal type.
+        //The difference is that Normal type is weak to Fighting, which is not
+        //used yet.
         var x = new Mechanics.DamageHandler();
 
         Assert.AreEqual(inputInt, x.DamageCheck(type, inputInt));
     }
+
+
+    [TestMethod]
+    [DataRow("Normal", 100, 100, 100, 100)]
+    [DataRow("Fire", 100, 200, 50, 50)]
+    [DataRow("Water", 100, 50, 50, 200)]
+    [DataRow("Grass", 100, 50, 200, 50)]
+    public void Test_DamageTypeModifiers(string type, double damageDealt, double grassExpected, double waterExpected, int fireExpected)
+    {
+        //Check that a pokemon with a type has different damage modifiers.
+        //For reference, each should be resistant to 2, weak to 1 and neutral to normal.
+
+        Bulbasaur grass = new Bulbasaur();  //Weak to fire
+        Squirtle water = new Squirtle();    //Weak to grass
+        Charmander fire = new Charmander();//Weak to water
+
+        //At the time of writing, TakeDamage does not store damage taken, or
+        //return any data. Test is written to expect data.
+        //Assert.AreEqual(grassExpected, grass.TakeDamage(type, damageDealt));
+        //Assert.AreEqual(waterExpected, water.TakeDamage(type, damageDealt));
+        //Assert.AreEqual(grassExpected, fire.TakeDamage(type, damageDealt));
+
+        //Forcing a throw at present to simulate this, given that this is a case
+        //where the test needs new functionality, not work on existing methods.
+        Assert.IsTrue(false);
+
+    }
+
+
+
+
 
 
 
--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
-As of right now, this is just a practice project for me to tinker with some  programming concepts that needed a bit more refinement.
+This is a practice project.
+
+The goals of this practice project are;
+1. To practically use Inheritance, Interfaces 
+2. To work on Unit Testing, and TTD
+3. To brush up on the finer points of enumeration
+4. To create a text-based "clone" of the pokemon system, adding features logically
+
+The reason I have chosen Pokemon as a base for what to make is because it's a system I understand well without referring to a document, and has many features to implement that are ideal for brushing up on Inheritance and Interfacing, such as elemental types that can be applied through Inheritance, or ensuring different Pokemon adhere to a common interface.
